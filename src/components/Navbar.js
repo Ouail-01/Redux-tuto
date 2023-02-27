4b7b6f78556f908be8aae113ce8de736945d9bbc
Ouail-01 <ouaillaarabi@gmail.com>
2023-02-27 21:00:45 +0100

    Complete the tutorial

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,22 @@
-import { CartIcon, Testing } from '../icons';
+import { CartIcon } from '../icons';
 import { useSelector } from 'react-redux';
 
 const Navbar = () => {
-  const { amount } = useSelector((state) => state.cart);
-
+  const { amount } = useSelector((store) => store.cart);
   return (
-    <nav>
-      <div className='nav-center'>
-        <h3>redux toolkit</h3>
-        <div className='nav-container'>
-          <Testing />
-          <CartIcon />
-          <div className='amount-container'>
-            <p className='total-amount'>{amount}</p>
+    <>
+      <nav>
+        <div className='nav-center'>
+          <h3>redux toolkit</h3>
+          <div className='nav-container'>
+            <CartIcon />
+            <div className='amount-container'>
+              <p className='total-amount'>{amount}</p>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 export default Navbar;
